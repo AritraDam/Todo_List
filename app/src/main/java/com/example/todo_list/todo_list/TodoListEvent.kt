55ddf075bc472f9ff016789cfd713941bb1caaad
unknown <aritrodam773@gmail.com>
2025-04-08 21:56:28 +0530

    Todo app, implemented room,dependency injections and viewModel

---
+++ app/src/main/java/com/example/todo_list/todo_list/TodoListEvent.kt
@@ -0,0 +1,11 @@
+package com.example.todo_list.todo_list
+
+import com.example.todo_list.data.Todo
+
+sealed class TodoListEvent {
+    data class DeleteTodo(val todo: Todo): TodoListEvent()
+    data class OnDoneChange(val todo: Todo, val isDone: Boolean) : TodoListEvent()
+    object OnUndoDeleteClick: TodoListEvent()
+    object OnAddTodoClick: TodoListEvent()
+    data class OnTodoClick(val todo: Todo): TodoListEvent()
+}
---
+++ app/src/main/java/com/example/todo_list/util/UiEvent.kt
@@ -0,0 +1,11 @@
+package com.example.todo_list.util
+
+sealed class UiEvent {
+    object PopBackStack: UiEvent()
+    data class Navigate(val route: String): UiEvent()
+
+    data class ShowSnackbar(
+        val message: String,
+        val action: String? = null
+    ): UiEvent()
+}
