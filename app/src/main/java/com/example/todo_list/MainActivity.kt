531a33980251ba8bae302d2730c5c3f06b5481a3
unknown <aritrodam773@gmail.com>
2025-04-12 14:19:32 +0530

    Added a splash screen and fixed minor ui bug

--- app/src/main/java/com/example/todo_list/MainActivity.kt
+++ app/src/main/java/com/example/todo_list/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -17,8 +19,14 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                viewModel.isLoading.value
+            }
+        }
         enableEdgeToEdge()
         setContent {
             Todo_ListTheme {
@@ -35,7 +43,7 @@ class MainActivity : ComponentActivity() {
                         )
                     }
                     composable(
-                        route = Routes.ADD_EDIT_TODO + "?todoId = {todoId}",
+                        route = Routes.ADD_EDIT_TODO + "?todoId={todoId}",
                         arguments = listOf(
                             navArgument(name = "todoId"){
                                 type = NavType.IntType
--- app/src/main/java/com/example/todo_list/todo_list/TodoListScreen.kt
+++ app/src/main/java/com/example/todo_list/todo_list/TodoListScreen.kt
@@ -1,8 +1,10 @@
 package com.example.todo_list.todo_list
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.FloatingActionButton
@@ -47,6 +49,7 @@ fun TodoListScreen(
             }
         }
     }
+    Spacer(modifier = Modifier.height(12.dp))
     Scaffold(
         scaffoldState = scaffoldState,
         floatingActionButton = {
